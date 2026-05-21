4943d84bc3ec9878dc430bb746d64a0f09383740
CoupDeGrace92 <nickoboy1992@gmail.com>
2026-05-21 17:16:06 -0500

    Added async crawler functionality with max pages param and a concurrent limit using pLimit

--- src/crawl.ts
+++ src/crawl.ts
@@ -1,10 +1,11 @@
 import { JSDOM } from "jsdom"
+import { url } from "node:inspector"
+import pLimit from "p-limit"
 
 export function normalizeURL(url: string): string {
     //here we are going to grab the protocol
     //Side benefit: Also validates that the string is a url - this will error if we feed it a non url string
     let urlObj = new URL(url)
-    console.log(urlObj.protocol)
     let out: string = urlObj.href.replace(urlObj.protocol + "//", "")
     if (out.endsWith("/")) {
         out = out.slice(0,-1)
@@ -86,7 +87,7 @@ export function extractPageData(html: string, pageURL: string): ExtractedPageDat
     return out
 }
 
-export async function getHTML(url: string){
+export async function getHTML(url: string): Promise<string> {
     const response = await fetch(url, {
         headers: {
             "User-Agent": "TSSimpleCrawl/1.0"
@@ -95,20 +96,138 @@ export async function getHTML(url: string){
 
     if (response.status >= 400) {
         console.error(`Returned error status: ${response.status} ${response.statusText}`)
-        return
+        return ""
     }
-    if (!response.headers || response.headers.get("content-type") !== "text/html" ) {
+    const contentType = response.headers.get("content-type")
+    if (!contentType || !contentType.includes("text/html") ) {
         console.error(`Response not content-type text/html`)
-        return
+        return ""
     }
-
-    console.log(await response.text() ?? "response body not found")
+    return await response.text() ?? ""
 }
 
 export async function crawlPage(
     baseURL: string, 
     currentURL: string = baseURL, 
     pages: Record<string, number> = {}
-) {
-    
+): Promise<Record<string, number>> {
+    const absolute = new URL(currentURL, baseURL).toString()
+    const normalized = normalizeURL(absolute)
+    pages[normalized] = (pages[normalized] ?? 0) + 1
+    const curURL = new URL(absolute)
+    const base = new URL(baseURL)
+    if (curURL.hostname !== base.hostname) {
+        return pages
+    }
+    if (pages[normalized] !== 1 ) {
+        return pages
+    }
+
+    const html = await getHTML(absolute)
+    if (!html) {
+        return pages
+    }
+    const infoObj = extractPageData(html, absolute)
+    for (let u of infoObj.outgoingLinks) {
+        await crawlPage(baseURL, u, pages)
+    }
+    return pages
 }
+
+export class ConcurrentCrawler {
+    private baseURL: string
+    private pages: Record<string, number>
+    private limit: <T>(fn: () => Promise<T>) => Promise<T>
+    private maxPages: number
+    private shouldStop: boolean
+    private allTasks: Set<Promise<void>>
+
+    constructor(baseURL: string, limit: number = 1, maxPages: number = 200, pages: Record<string, number> = {}) {
+        this.limit = pLimit(limit)
+        this.baseURL = baseURL
+        this.pages = {}
+        this.maxPages = maxPages
+        this.shouldStop = false
+        this.allTasks = new Set<Promise<void>>
+    }
+    private stop() {
+        this.shouldStop = true
+    }
+
+    private addPageVisit(normalizedURL: string): boolean {
+        if (this.shouldStop) {
+            return false
+        }
+        this.pages[normalizedURL] = (this.pages[normalizedURL] ?? 0) + 1
+        if (this.pages[normalizedURL] === 1){
+            if (Object.entries(this.pages).length >= this.maxPages){
+                this.stop()
+                console.log("Reached maximum number of pages to crawl")
+                return false
+            }
+            return true
+        }
+        return false
+    }
+
+    private async getHTML(currentURL: string): Promise<string> {
+        return await this.limit(async () => {
+            const html = await getHTML(currentURL)
+            if (!html) {
+                throw new Error("No HTML returned")
+            }
+            return html
+        })
+    }
+
+    private async crawlPage(currentURL: string): Promise<void> {
+        if (this.shouldStop) {
+            return
+        }
+        const curURL = new URL(currentURL, this.baseURL)
+        const absolute = curURL.toString()
+        const normalized = normalizeURL(absolute)
+        const base = new URL(this.baseURL) //we could probably make a performance improvement by making this a class var
+        
+        if (!this.addPageVisit(normalized)) {
+            return
+        }
+        
+        if (curURL.hostname !== base.hostname) {
+            return
+        }
+        let html: string
+
+        try{
+            html = await this.getHTML(absolute)
+        } catch (err) {
+            console.error(err)
+            return
+        }
+
+        if (!html) {
+            return
+        }
+        const infoObj = extractPageData(html, absolute)
+        for (let u of infoObj.outgoingLinks) {
+            const task = this.crawlPage(u)
+            this.allTasks.add(task)
+            task.finally(() => this.allTasks.delete(task))
+        }
+        return
+    }
+
+    async crawl(): Promise<Record<string,number>> {
+        await this.crawlPage(this.baseURL)
+        while (this.allTasks.size > 0){
+            await Promise.all([...this.allTasks])
+        }
+        return this.pages
+    }
+}
+
+export async function crawlSiteAsync(baseURL: string, limit: number = 1, maxPages: number = 100): Promise<Record<string, number>> {
+    const myCrawler = new ConcurrentCrawler(baseURL, limit, maxPages)
+    return await myCrawler.crawl()
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
-import { getHTML } from "./crawl.js"
+import { crawlPage, crawlSiteAsync } from "./crawl.js"
 
 async function main(){
-    if (!process.argv.slice(2) || process.argv.slice(2).length !== 1){
-        console.error("Incorrect number of args - expect just a url")
+    if (!process.argv.slice(2) || process.argv.slice(2).length > 3){
+        console.error("Incorrect number of args - a URL, LIMIT, and MAXCONCURRENCY")
         process.exit(1)
     }
     const baseURL = process.argv.slice(2)[0]
+    const limit = Number(process.argv.slice(2)[1])
+    if (Number.isNaN(limit)){
+        console.error("Limit term is not a number - command usage: npm start URL LIMIT MAXCONCURRENCY")
+    }
+    const maxCon = Number(process.argv.slice(2)[2])
+    if (Number.isNaN(maxCon)){
+        console.error("Max concurrency is not a number - command usage: npm start URL LIMIT MAXCONCURRENCY")
+    }
     console.log(`Crawler starting at: ${baseURL}`)
+    let pages: Record<string, number> = {} 
     try{
-        await getHTML(baseURL)
+        pages = await crawlSiteAsync(baseURL, limit, maxCon)
     } catch (err) {
         console.error(err)
     }
+    console.log(pages)
     process.exit(0)
 }
 
